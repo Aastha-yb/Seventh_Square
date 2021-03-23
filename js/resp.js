8165cd1b26fff8d73b967c5dccc050798a5e9ee7
Aastha2891 <aasthasingh507@gmail.com>
2021-03-24 01:55:46 +0530

    Updating Slidemenu

--- js/resp.js
+++ js/resp.js
@@ -1,13 +1,13 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-a = document.querySelectorAll('.navbar .options')
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// a = document.querySelectorAll('.navbar .options')
 
-burger.addEventListener('click',()=>{
-    for (i = 0; i < a.length; i++) {
-        a[i].classList.toggle('v-class-resp');
-    }
-    navbar.classList.toggle('h-nav-resp');
-})
+// burger.addEventListener('click',()=>{
+//     for (i = 0; i < a.length; i++) {
+//         a[i].classList.toggle('v-class-resp');
+//     }
+//     navbar.classList.toggle('h-nav-resp');
+// })
 
 function myFunction() {
     var x = document.getElementById("password-field");
@@ -23,3 +23,13 @@ function myFunction_two() {
         x.style.display = "block";
     
 }
+function myFunction_three() {
+    var x = document.getElementById("menu");
+        x.style.display = "block";
+    
+}
+$( document ).ready(function() {
+    $('#jsModal').on('click', function(){
+      $('#myModal').modal()
+    });
+  });
