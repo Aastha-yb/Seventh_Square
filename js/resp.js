dad0b46bef7f7e11865d2fb3d3d099ad8de352ea
Aastha2891 <aasthasingh507@gmail.com>
2021-03-27 18:00:53 +0530

    Adding Dashboard.html

--- js/resp.js
+++ js/resp.js
@@ -9,6 +9,15 @@
 //     navbar.classList.toggle('h-nav-resp');
 // })
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("header").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("header").style.marginLeft= "0";
+};
 function myFunction() {
     var x = document.getElementById("password-field");
     if (x.type === "password") {
